8711fc1a1ea10565334c5981f9ee8ff3284cc628
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-05-22 13:49:24 +0200

    conlicts resolved

--- Android/Application/src/gcm/android/parser/FeedView.java
+++ Android/Application/src/gcm/android/parser/FeedView.java
@@ -2,11 +2,13 @@ package gcm.android.parser;
 
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.EditText;
 
 public class FeedView {
 	public TextView pubDate;
 	public TextView title;
 	public TextView creator;
 	public TextView link;
+	public EditText editText;
 	public ImageView image;
 }
