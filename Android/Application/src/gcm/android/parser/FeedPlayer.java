17e837eed4c2671ca63fe3719fe6b9309f655c01
Paul MAYER <paul.mayer@ensimag.imag.fr>
2012-05-22 17:24:07 +0200

    notifications

--- Android/Application/src/gcm/android/parser/FeedPlayer.java
+++ Android/Application/src/gcm/android/parser/FeedPlayer.java
@@ -2,7 +2,14 @@ package gcm.android.parser;
 
 import java.util.ArrayList;
 
+import javax.xml.parsers.SAXParserFactory;
+
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +45,42 @@ public class FeedPlayer extends Activity {
 
 			((TextView)findViewById(R.id.title)).setText(((Feed)parent.getItemAtPosition(position)).getTitle());
 			((TextView)findViewById(R.id.description)).setText(((Feed)parent.getItemAtPosition(position)).getDescription());
+		
+			
+			 /* Get the notification manager  */
+	         NotificationManager mNotManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+	         /* Create a notification */
+	         String MyText = "Text inside: By Firstdroid.com";
+	         Notification mNotification = new Notification(
+	                R.drawable.icon,                // An Icon to display
+	                MyText,                         // the text to display in the ticker
+	                System.currentTimeMillis()       ); // the time for the notification
+
+	         /* Starting an intent */
+	         String MyNotifyTitle = "Firstdroid Rocks!!!";
+	         String MyNotifiyText  = "Firstdroid: our forum at www.firstdroid.com";
+	         //Intent MyIntent = new Intent(getApplicationContext(),parser.class);
+	         Intent MyIntent = new Intent();
+	         MyIntent.putExtra("extendedTitle", MyNotifyTitle);
+	         MyIntent.putExtra("extendedText" , MyNotifiyText);
+	         PendingIntent StartIntent = PendingIntent.getActivity(  getApplicationContext(),
+	                                                   0,
+	                                                   MyIntent,
+	                                                   0);
+
+	         /* Set notification message */
+	         mNotification.setLatestEventInfo(   getApplicationContext(),
+	                                    MyNotifyTitle,
+	                                    MyNotifiyText,
+	                                    StartIntent);
+
+	         mNotification.ledOnMS  = 200;    //Set led blink (Off in ms)
+	         mNotification.ledOffMS = 200;    //Set led blink (Off in ms)
+	         mNotification.ledARGB = 40;   //Set led color
+
+	         /* Sent Notification to notification bar */
+	         mNotManager.notify(  0 , mNotification );
 		}
     };
 
--- Android/Application/src/gcm/android/parser/FeedView.java
+++ Android/Application/src/gcm/android/parser/FeedView.java
@@ -1,8 +1,6 @@
 package gcm.android.parser;
 
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.EditText;
 
 public class FeedView {
 	public TextView pubDate;
