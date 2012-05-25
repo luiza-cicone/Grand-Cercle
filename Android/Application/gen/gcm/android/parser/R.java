e9a9401ca0e817c2563cbf969edb7723d843371b
Paul MAYER <paul.mayer@ensimag.imag.fr>
2012-05-25 18:18:45 +0200

    ok apli

--- Android/Application/gen/gcm/android/parser/R.java
+++ Android/Application/gen/gcm/android/parser/R.java
@@ -11,24 +11,18 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int essai=0x7f020000;
-        public static final int header=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int icon=0x7f020003;
+        public static final int header=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f050005;
-        public static final int creator=0x7f050006;
-        public static final int linearLayout1=0x7f050000;
-        public static final int linearLayout2=0x7f050001;
-        public static final int linearLayout3=0x7f050002;
-        public static final int linearLayout5=0x7f050004;
-        public static final int link=0x7f050009;
-        public static final int listFeed=0x7f05000b;
-        public static final int pub_date=0x7f050008;
-        public static final int recherche=0x7f05000a;
-        public static final int tableRow1=0x7f050003;
-        public static final int title=0x7f050007;
+        public static final int creator=0x7f050002;
+        public static final int description=0x7f050001;
+        public static final int link=0x7f050004;
+        public static final int listFeed=0x7f050006;
+        public static final int pub_date=0x7f050003;
+        public static final int recherche=0x7f050005;
+        public static final int title=0x7f050000;
     }
     public static final class layout {
         public static final int content_view=0x7f030000;
