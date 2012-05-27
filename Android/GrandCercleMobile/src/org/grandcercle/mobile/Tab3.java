4a9c9cc85da746b4a87ce7aaac3020767cb5a398
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-05-27 12:14:52 +0200

    ajout fichiers onglets

---
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/Tab3.java
@@ -0,0 +1,20 @@
+package org.grandcercle.mobile;
+
+
+
+
+import org.grandcercle.mobile.R;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+public class Tab3 extends Activity {
+	 @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab3);
+	 }
+}
---
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/Tab5.java
@@ -0,0 +1,20 @@
+package org.grandcercle.mobile;
+
+
+
+
+import org.grandcercle.mobile.R;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+public class Tab5 extends Activity {
+	 @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tab5);
+	 }
+}
