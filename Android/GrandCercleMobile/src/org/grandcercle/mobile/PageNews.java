becbeeb741f20a7f2a9f29cdd5a6876d30ac2b9b
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-05-28 17:43:20 +0200

    ok

--- Android/GrandCercleMobile/src/org/grandcercle/mobile/PageNews.java
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/PageNews.java
@@ -1,10 +1,6 @@
 package org.grandcercle.mobile;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
+
 
 import android.app.Activity;
 import android.os.Bundle;
