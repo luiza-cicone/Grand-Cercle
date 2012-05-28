14efe1e8d2e4e38639166c114905d5229bc02368
Jérémy Luquet <jeremy.luquet@ensimag.imag.fr>
2012-05-28 13:51:57 +0200

    ok

--- Android/GrandCercleMobile/gen/org/grandcercle/mobile/R.java
+++ Android/GrandCercleMobile/gen/org/grandcercle/mobile/R.java
@@ -26,25 +26,31 @@ public final class R {
         public static final int cal_left_arrow_on=0x7f020003;
         public static final int cal_right_arrow_off=0x7f020004;
         public static final int cal_right_arrow_on=0x7f020005;
-        public static final int calendar_bar=0x7f020006;
-        public static final int calendar_bg_orange=0x7f020007;
-        public static final int calendar_button_selector=0x7f020008;
-        public static final int calendar_centralheader=0x7f020009;
-        public static final int calendar_left_arrow_selector=0x7f02000a;
-        public static final int calendar_right_arrow_selector=0x7f02000b;
-        public static final int calendar_tile=0x7f02000c;
-        public static final int calendar_tile_small=0x7f02000d;
-        public static final int calendar_top_header=0x7f02000e;
-        public static final int calendar_view=0x7f02000f;
-        public static final int essai=0x7f020010;
-        public static final int grand_cercle=0x7f020011;
-        public static final int header=0x7f020012;
-        public static final int ic_launcher=0x7f020013;
-        public static final int icon=0x7f020014;
-        public static final int tab_bg_selected=0x7f020015;
-        public static final int tab_bg_selector=0x7f020016;
-        public static final int tab_bg_unselected=0x7f020017;
-        public static final int tab_text_selector=0x7f020018;
+        public static final int calendar=0x7f020006;
+        public static final int calendar_bar=0x7f020007;
+        public static final int calendar_bg_orange=0x7f020008;
+        public static final int calendar_button_selector=0x7f020009;
+        public static final int calendar_centralheader=0x7f02000a;
+        public static final int calendar_left_arrow_selector=0x7f02000b;
+        public static final int calendar_right_arrow_selector=0x7f02000c;
+        public static final int calendar_tile=0x7f02000d;
+        public static final int calendar_tile_small=0x7f02000e;
+        public static final int calendar_top_header=0x7f02000f;
+        public static final int calendar_view=0x7f020010;
+        public static final int deals=0x7f020011;
+        public static final int essai=0x7f020012;
+        public static final int grand_cercle=0x7f020013;
+        public static final int header=0x7f020014;
+        public static final int ic_launcher=0x7f020015;
+        public static final int icon=0x7f020016;
+        public static final int icons=0x7f020017;
+        public static final int news=0x7f020018;
+        public static final int search=0x7f020019;
+        public static final int settings=0x7f02001a;
+        public static final int tab_bg_selected=0x7f02001b;
+        public static final int tab_bg_selector=0x7f02001c;
+        public static final int tab_bg_unselected=0x7f02001d;
+        public static final int tab_text_selector=0x7f02001e;
     }
     public static final class id {
         public static final int button1=0x7f070004;
@@ -54,6 +60,7 @@ public final class R {
         public static final int creator=0x7f070005;
         public static final int currentMonth=0x7f07000a;
         public static final int description=0x7f070003;
+        public static final int icone=0x7f070010;
         public static final int link=0x7f070007;
         public static final int listFeed=0x7f07000e;
         public static final int nextMonth=0x7f07000b;
@@ -62,7 +69,7 @@ public final class R {
         public static final int pub_date=0x7f070006;
         public static final int selectedDayMonthYear=0x7f070008;
         public static final int tabsLayout=0x7f07000f;
-        public static final int tabsText=0x7f070010;
+        public static final int tabsText=0x7f070011;
         public static final int title=0x7f070002;
     }
     public static final class layout {
@@ -71,13 +78,12 @@ public final class R {
         public static final int feed_view=0x7f030002;
         public static final int main=0x7f030003;
         public static final int simple_calendar_view=0x7f030004;
-        public static final int tab=0x7f030005;
-        public static final int tab1=0x7f030006;
-        public static final int tab2=0x7f030007;
-        public static final int tab3=0x7f030008;
-        public static final int tab4=0x7f030009;
-        public static final int tab5=0x7f03000a;
-        public static final int tab_item=0x7f03000b;
+        public static final int tab1=0x7f030005;
+        public static final int tab2=0x7f030006;
+        public static final int tab3=0x7f030007;
+        public static final int tab4=0x7f030008;
+        public static final int tab5=0x7f030009;
+        public static final int tab_item=0x7f03000a;
     }
     public static final class string {
         public static final int app_name=0x7f050004;
--- Android/GrandCercleMobile/src/org/grandcercle/mobile/GCM.java
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/GCM.java
@@ -12,8 +12,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GCM extends TabActivity {
     /** Called when the activity is first created. */
@@ -23,6 +26,7 @@ public class GCM extends TabActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Intent intent;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
@@ -30,12 +34,33 @@ public class GCM extends TabActivity {
 		
 		// Décommentez la ligne suivante pour séparer vos onglets via une image
         //this.tabHost.getTabWidget().setDividerDrawable(R.drawable.tab_divider);
-
+		intent = new Intent().setClass(this, Tab1.class);
+		//intent.getAction();
+		//tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator( "Evenement",getResources().getDrawable(R.drawable.deals)).setContent(intent));
         setupTab("Evenement", "tab1", new Intent().setClass(this, Tab1.class));
+		//intent = new Intent().setClass(this, Tab2.class);
+		//tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator( "News",getResources().getDrawable(R.drawable.news)).setContent(intent));
         setupTab("News", "tab2", new Intent().setClass(this, Tab2.class));
+		//intent = new Intent().setClass(this, Tab3.class);
+		//tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator( "Calendrier",getResources().getDrawable(R.drawable.calendar)).setContent(intent));
         setupTab("Calendrier", "tab3", new Intent().setClass(this, Tab3.class));
+		//intent = new Intent().setClass(this, Tab4.class);
+		//tabHost.addTab(tabHost.newTabSpec("tab4").setIndicator( "Boutique",getResources().getDrawable(R.drawable.settings)).setContent(intent));
         setupTab("Boutique", "tab4", new Intent().setClass(this, Tab4.class));
+		//intent = new Intent().setClass(this, Tab5.class);
+		//tabHost.addTab(tabHost.newTabSpec("tab5").setIndicator( "Info",getResources().getDrawable(R.drawable.search)).setContent(intent));
         setupTab("Infos", "tab5", new Intent().setClass(this, Tab5.class));
+        
+        
+        /*tabHost.setOnTabChangedListener(
+        		new TabHost.OnTabChangeListener (){
+        		public void onTabChanged(String tabId){
+        			Toast.makeText(GCM.this, "L’onglet avec l’identifiant : "+ tabId + " a été cliqué", Toast.LENGTH_SHORT).show();
+
+        		}
+        	}
+        );*/
+
 	}
 	
 	  private void setupTab(String name, String tag, Intent intent) {
@@ -43,10 +68,11 @@ public class GCM extends TabActivity {
 		}
 	  
 	  private static View createTabView(final Context context, final String text) {
-			View view = LayoutInflater.from(context).inflate(R.layout.tab_item, null);
+			View view = LayoutInflater.from(context).inflate(R.layout.tab_item, null);	
 			TextView tv = (TextView) view.findViewById(R.id.tabsText);
+			//ImageView im = (ImageView) view.findViewById(R.drawable.calendar);
 			tv.setText(text);
-
+			//(im.getResources().getIdentifier(text, "drawable", "org.grandcercle.mobile"));
 			return view;
 		}
 	  
