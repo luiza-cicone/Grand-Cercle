0d780fea0544b9ac84e281820b8eb835e8e1e1eb
Luiza CICONE <luiza.cicone@gmail.com>
2012-05-28 20:44:12 +0200

    stuff

---
+++ iOS/Grand Cercle/Grand CercleTests/Grand_CercleTests.h
@@ -0,0 +1,13 @@
+//
+//  Grand_CercleTests.h
+//  Grand CercleTests
+//
+//  Created by Luiza Cicone on 28/5/12.
+//  Copyright (c) 2012 Ensimag. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Grand_CercleTests : SenTestCase
+
+@end
---
+++ iOS/Grand Cercle/Grand CercleTests/Grand_CercleTests.m
@@ -0,0 +1,32 @@
+//
+//  Grand_CercleTests.m
+//  Grand CercleTests
+//
+//  Created by Luiza Cicone on 28/5/12.
+//  Copyright (c) 2012 Ensimag. All rights reserved.
+//
+
+#import "Grand_CercleTests.h"
+
+@implementation Grand_CercleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Grand CercleTests");
+}
+
+@end
