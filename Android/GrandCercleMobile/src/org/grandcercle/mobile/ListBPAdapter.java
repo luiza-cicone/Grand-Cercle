d8de31b25ff4571dddbb425cbf1bd3cdda9bf2b5
Paul MAYER <paul.mayer@ensimag.imag.fr>
2012-05-31 15:55:24 +0200

    new files

---
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/ListBPAdapter.java
@@ -0,0 +1,80 @@
+package org.grandcercle.mobile;
+
+import java.util.ArrayList;
+import org.grandcercle.mobile.R;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ListBPAdapter extends BaseAdapter {
+
+	// les données à afficher
+	private ArrayList<BP> listBP;
+	
+	/* Le LayoutInflater permet de parser un layout XML et de 
+	 * le transcoder en IHM Android. Pour respecter la classe 
+	 * BaseAdapter
+	 */
+	private LayoutInflater inflater;
+	
+	public ListBPAdapter(Context context,ArrayList<BP> listBP) {
+		inflater = LayoutInflater.from(context);
+		this.listBP = listBP;
+	}
+	
+	/* il nous faut spécifier la méthode "getCount()". 
+	 * Cette méthode permet de connaître le nombre d'items présent 
+	 * dans la liste. Dans notre cas, il faut donc renvoyer le nombre
+	 * de personnes contenus dans "mListP".
+	 */
+	public int getCount() {
+		return listBP.size();
+	}
+
+	// Permet de retourner un objet contenu dans la liste
+	public BP getItem(int index) {
+		return listBP.get(index);
+	}
+
+	public long getItemId(int index) {
+		return this.listBP.get(index).getId();
+		
+	}
+	
+	/* Le paramètre "convertView" permet de recycler les élements 
+	 * de notre liste. En effet, l'opération pour convertir un layout 
+	 * XML en IHM standard est très couteuse pour la plateforme Android. 
+	 * On nous propose ici de réutiliser des occurences créées qui ne sont 
+	 * plus affichées. Donc si ce paramètre est "null" alors, il faut "inflater" 
+	 * notre layout XML, sinon on le réutilise
+	 */
+	public View getView(int position, View convertView, ViewGroup parent){
+		BPView bpv;		
+		
+		if (convertView == null) {
+			bpv = new BPView();
+			convertView = inflater.inflate(R.layout.cell_bons_plans,null);
+
+			bpv.title = (TextView)convertView.findViewById(R.id.title);			
+			bpv.description = (TextView)convertView.findViewById(R.id.description);
+			bpv.image = (ImageView)convertView.findViewById(R.id.image);
+			convertView.setTag(bpv);
+
+		} else {
+			bpv = (BPView)convertView.getTag();
+		}
+		
+		bpv.title.setText(listBP.get(position).getTitle());
+		bpv.description.setText(listBP.get(position).getDescription());
+		// image stockée infiniement, car BP ont une grande durée de vie
+		UrlImageViewHelper.setUrlDrawable(bpv.image,listBP.get(position).getImage(),R.drawable.loading,UrlImageViewHelper.CACHE_DURATION_INFINITE);
+		
+		return convertView;
+	}
+
+}
---
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/ParserXMLHandlerBP.java
@@ -0,0 +1,97 @@
+package org.grandcercle.mobile;
+
+import java.util.ArrayList;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+public class ParserXMLHandlerBP extends ParserXMLHandler {
+	private final String IMAGE = "image";
+	
+	// Array list d'evenements
+	private ArrayList<BP> listBP;
+	
+	// Boolean permettant de savoir si nous sommes à l'intérieur d'une news
+	private boolean inBP;
+
+	// Feed courant
+	private BP currentBP;
+	
+	
+	/* Cette méthode est appelée par le parser une et une seule  
+	 * fois au démarrage de l'analyse de votre flux xml. 
+	 * Elle est appelée avant toutes les autres méthodes de l'interface,  
+	 * à l'exception unique, évidemment, de la méthode setDocumentLocator. 
+	 * Cet événement devrait vous permettre d'initialiser tout ce qui doit 
+	 * l'être avant le début du parcours du document.
+	 */ 
+	@Override
+	public void startDocument() throws SAXException {
+		super.startDocument();
+		listBP = new ArrayList<BP>();
+	}
+	
+	/* 
+	 * Fonction étant déclenchée lorsque le parser trouve un tag XML
+	 * C'est cette méthode que nous allons utiliser pour instancier un nouveau feed
+ 	*/
+	@Override
+	public void startElement(String uri, String localName, String name,	Attributes attributes) throws SAXException {
+		// Nous réinitialisons le buffer a chaque fois qu'il rencontre un node
+		buffer = new StringBuffer();		
+		
+		// Ci dessous, localName contient le nom du tag rencontré
+		
+		// Nous avons rencontré un tag NADE, il faut donc instancier un nouvel BP		
+		if (localName.equalsIgnoreCase(NODE)){
+			this.currentBP = new BP();
+			inBP = true;
+		}
+	}
+	
+	 
+	/* Fonction étant déclenchée lorsque le parser à parsé 	
+	 * l'intérieur de la balise XML La méthode characters  
+	 * a donc fait son ouvrage et tous les caractères inclus 
+	 * dans la balise en cours sont copiés dans le buffer 
+	 * On peut donc tranquillement les récupérer pour compléter
+	 * notre objet currentFeed
+	 */
+	@Override
+	public void endElement(String uri, String localName, String name) throws SAXException {		
+		
+		if (localName.equalsIgnoreCase(TITLE)){
+			if(inBP) {				
+				// Les caractères sont dans l'objet buffer
+				this.currentBP.setTitle(buffer.toString());				
+				buffer = null;
+			}
+		}
+		if(localName.equalsIgnoreCase(DESCRIPTION)){
+			if(inBP) {				
+				this.currentBP.setDescription(buffer.toString());				
+				buffer = null;
+			}
+		}
+		if (localName.equalsIgnoreCase(LINK)){
+			if(inBP) {				
+				this.currentBP.setLink(buffer.toString());				
+				buffer = null;
+			}
+		}
+		if (localName.equalsIgnoreCase(IMAGE)) {
+			if (inBP) {
+				this.currentBP.setImage(buffer.toString());
+				buffer = null;
+			}
+		}
+		if (localName.equalsIgnoreCase(NODE)){		
+			listBP.add(currentBP);
+			inBP = false;
+		}
+	}
+	// cette méthode nous permettra de récupérer les données
+	public ArrayList<BP> getListBP(){
+		return listBP;
+	}
+}
