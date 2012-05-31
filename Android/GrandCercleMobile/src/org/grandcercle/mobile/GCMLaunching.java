4c555b1358ac8407d5d55ae94d389764fa7a250e
Paul MAYER <paul.mayer@ensimag.imag.fr>
2012-06-01 00:49:08 +0200

    ok

--- Android/GrandCercleMobile/src/org/grandcercle/mobile/GCMLaunching.java
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/GCMLaunching.java
@@ -2,6 +2,7 @@ package org.grandcercle.mobile;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ProgressBar;
@@ -14,6 +15,7 @@ public class GCMLaunching extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.loading);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		progressBar = (ProgressBar)findViewById(R.id.progressBar);
 		progressBar.setProgress(0);
 		new LoadingPage().execute((Void)null);
