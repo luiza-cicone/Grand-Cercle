fe5f4d52dfc922acf15ea6ca665ee7cd7b0458f0
Jérémy KREIN <jeremy.krein@ensimag.imag.fr>
2012-06-01 17:27:11 +0200

    Ajout de la vue par 4

---
+++ iOS/Grand Cercle/EventFourNextViewController.m
@@ -0,0 +1,44 @@
+//
+//  EventFourNextViewController.m
+//  Grand Cercle
+//
+//  Created by Jérémy Krein on 01/06/12.
+//  Copyright (c) 2012 Ensimag. All rights reserved.
+//
+
+#import "EventFourNextViewController.h"
+
+@interface EventFourNextViewController ()
+
+@end
+
+@implementation EventFourNextViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
