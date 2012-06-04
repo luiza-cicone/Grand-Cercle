ee754e85ff0e4935334f0b29f515db8d432a907c
Luiza CICONE <luiza.cicone@gmail.com>
2012-06-04 10:44:50 +0200

    preferences

---
+++ iOS/Grand Cercle/Grand Cercle/PreferencesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesViewController.h
+//  Grand Cercle
+//
+//  Created by Luiza Cicone on 4/6/12.
+//  Copyright (c) 2012 Ensimag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreferencesViewController : UITableViewController
+
+@end
