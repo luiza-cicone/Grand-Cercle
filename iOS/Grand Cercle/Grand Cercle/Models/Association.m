4790b1878cc06b05acb682e2036ec0a39c28f8f7
Luiza CICONE <luiza.cicone@gmail.com>
2012-06-07 14:27:34 +0200

    sketch

---
+++ iOS/Grand Cercle/Grand Cercle/Models/Association.m
@@ -0,0 +1,20 @@
+//
+//  Association.m
+//  Grand Cercle
+//
+//  Created by Luiza Cicone on 6/6/12.
+//  Copyright (c) 2012 Ensimag. All rights reserved.
+//
+
+#import "Association.h"
+
+
+@implementation Association
+
+@dynamic name;
+@dynamic logo;
+@dynamic email;
+@dynamic events;
+@dynamic news;
+
+@end
