28e76fba7b43ac5c3539ca74c7faea3b96bc2a02
Paul MAYER <paul.mayer@ensimag.imag.fr>
2012-06-10 12:45:37 +0200

    ok

--- Android/GrandCercleMobile/src/org/grandcercle/mobile/PageBP.java
+++ Android/GrandCercleMobile/src/org/grandcercle/mobile/PageBP.java
@@ -7,7 +7,6 @@ import android.text.Html;
 import android.text.Spanned;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 /*
